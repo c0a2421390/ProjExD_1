41ec4def9882095a46a10d693b7c23ed8bc426e3
c0a2421390 <c0a2421390@edu.teu.ac.jp>
2025-11-18 17:11:23 +0900

    dev:こうかとんの移動処理の実装

--- pygame_image.py
+++ pygame_image.py
@@ -15,17 +15,29 @@ def main():
     bg_img_2 = pg.image.load("fig/pg_bg.jpg")
     kokaton_img = pg.image.load("fig/3.png")
     kokaton_img = pg.transform.flip(kokaton_img, True, False)
+    kokaton_rct = kokaton_img.get_rect()
+    kokaton_rct.center = 300, 200
     tmr = 0
     bg_val = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kokaton_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:
+            kokaton_rct.move_ip(0, 1)
+        if key_lst[pg.K_LEFT]:
+            kokaton_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            kokaton_rct.move_ip(1, 0)
+
         screen.blit(bg_img, [-bg_val, 0])
         screen.blit(bg_img_flipped, [1600 - bg_val, 0])
         screen.blit(bg_img_2, [3200 - bg_val, 0])
         
-        screen.blit(kokaton_img, [300, 200])
+        screen.blit(kokaton_img, kokaton_rct)
         pg.display.update()
         tmr += 1
         bg_val += 5
